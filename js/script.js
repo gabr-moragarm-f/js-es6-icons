62a7753cc6b26fbe171622c762f9b6a9ca9190a0
Gabriel Moraga <agarom97@gmail.com>
2021-01-21 17:00:51 +0100

    milestone 2 (?) + milestone 3

--- js/script.js
+++ js/script.js
@@ -74,38 +74,113 @@ const icons = [
   {
     name : 'user-astronaut',
     prefix : 'fa-',
-    type : 'user',
+    type : 'users',
     family: 'fas'
   },
   {
     name : 'user-graduate',
     prefix : 'fa-',
-    type : 'user',
+    type : 'users',
     family: 'fas'
   },
   {
     name : 'user-ninja',
     prefix : 'fa-',
-    type : 'user',
+    type : 'users',
     family: 'fas'
   },
   {
     name : 'user-secret',
     prefix : 'fa-',
-    type : 'user',
+    type : 'users',
     family: 'fas'
   }
-]
+];
+
+const colors = ['#AF936C', '#4ABE02', '#0000A7'];
 
 const containerHTML = document.getElementsByClassName('container')[0]
 
 icons.forEach((element) => {
   const {name, prefix, type, family} = element;
 
+  let iconColor;
+
+  switch (type) {
+    case 'animals':
+    iconColor = colors[0];
+    break;
+    case 'vegetables&fruits':
+    iconColor = colors[1];
+    break;
+    case 'users':
+    iconColor = colors[2];
+    break;
+    default:
+    console.error('default');
+  }
+
   containerHTML.innerHTML += `
   <div>
-    <i class="${family} ${prefix}${name}""></i>
-    <div>${name}</div>
+  <i class="${family} ${prefix}${name}" style="color:${iconColor}"></i>
+  <div>${name}</div>
   </div>
   `
+});
+
+const typeSelector = document.getElementsByClassName('type-selector')[0];
+
+const options = []
+
+icons.forEach((element) => {
+  const {type} = element;
+
+  if (!options.includes(type)) {
+    options.push(type)
+  }
+});
+
+options.forEach((element) => {
+  typeSelector.innerHTML += `
+  <option value="${element}">${element}</option>
+  `
+});
+
+const button = document.getElementsByClassName('btn')[0];
+
+button.addEventListener('click', () => {
+  containerHTML.innerHTML = '';
+
+  const filteredArray = icons.filter((element, index, array) => {
+    return element.type === typeSelector.value
+  });
+
+  filteredArray.forEach((element) => {
+    const {name, prefix, type, family} = element;
+
+    let iconColor;
+
+    switch (type) {
+      case 'animals':
+      iconColor = colors[0];
+      break;
+      case 'vegetables&fruits':
+      iconColor = colors[1];
+      break;
+      case 'users':
+      iconColor = colors[2];
+      break;
+      default:
+      console.error('default');
+    }
+
+
+
+    containerHTML.innerHTML += `
+    <div>
+    <i class="${family} ${prefix}${name}" style="color:${iconColor}"></i>
+    <div>${name}</div>
+    </div>
+    `
+  });
 });
